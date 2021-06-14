a7bc0e2c3302bb18f94ae2e326eae07f3265a172
Joeri Lock <joeri.lock@live.nl>
2021-06-14 13:02:43 +0200

    added a second page

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Joeri is buidling stuff'));
+app.get('/', (req, res) => res.send('I made a second page'));
+app.get('/second', (req, res) => res.send('you found the second page'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
